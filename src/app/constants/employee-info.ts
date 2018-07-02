eadf146f6868f974f29ffae6690f04ee2bacbf53
chandra jha <jamresh91@gmail.com>
2018-07-02 15:32:32 +0530

    2nd commit

--- src/app/constants/employee-info.ts
+++ src/app/constants/employee-info.ts
@@ -1,128 +1,45 @@
 import { EmployeeDetail } from '../modals/employe-modal';
 
 export const employeesData: EmployeeDetail[] = [
-    {
-        eid: 1, ename: 'Rajneesh K P', desig: 'developer', mob: 9471931608,
-        email: 'rkp321@gmail.com', loc: 'saharsa', mgrname: 'S.RadhaKrishna', mid: 1
-      },
-      {
-        eid: 1, ename: 'Sumant kumar', desig: 'TeamLeader', mob: 9371932347,
-        email: 'ksumant2@gmail.com', loc: 'supaul', mgrname: 'BalaThirumurgan', mid: 2
-      },
-      {
-        eid: 1, ename: 'Mayank Tripathi', desig: 'programmer', mob: 8871931623,
-        email: 'tripathi3@gmail.com', loc: 'Bhelore', mgrname: 'K Kiran', mid: 4
-      },
-      {
-        eid: 1, ename: 'Rohan Gokhale', desig: 'Executive', mob: 9477771608,
-        email: 'gokhale43@gmail.com', loc: 'Chennai', mgrname: 'K Kiran', mid: 4
-      },
-      {
-        eid: 1, ename: 'Prashant Kishore', desig: 'Programmer', mob: 9471881611, email:
-          'kispra23@gmail.com', loc: 'Pondicherry', mgrname: 'Prakash Khan', mid: 3
-      },
-      {
-        eid: 1, ename: 'k Rambabu', desig: 'programmer', mob: 9231931608,
-        email: 'rambabu65@gmail.com', loc: 'Madhepura', mgrname: 'Prakash Khan', mid: 3
-      },
-      {
-        eid: 1, ename: 'SundarKant', desig: 'designer', mob: 9123931608,
-        email: 'sukant7@gmail.com', loc: 'Mohali', mgrname: 'Prakash Khan', mid: 3
-      },
-      {
-        eid: 1, ename: 'Raghvendra', desig: 'designer', mob: 9471932123,
-        email: 'raghuram8@gmail.com', loc: 'Ranchi', mgrname: 'S.RadhaKrishna', mid: 1
-      },
-      {
-        eid: 1, ename: 'Mathili Thakur', desig: 'Singer', mob: 9471911656,
-        email: 'mathili9@gmail.com', loc: 'Darbhanga', mgrname: 'Prakash Khan', mid: 2
-      },
-      {
-        eid: 1, ename: 'Ayachi Thakur', desig: 'Writer', mob: 9471941543,
-        email: 'ayanchi010@gmail.com', loc: 'Supaul', mgrname: 'S.RadhaKrishna', mid: 1
-      },
-      {
-        eid: 1, ename: 'Mathili Thakur', desig: 'Singer', mob: 9471936656,
-        email: 'mathili9@gmail.com', loc: 'Darbhanga', mgrname: 'S.RadhaKrishna', mid: 2
-      },
-      {
-        eid: 1, ename: 'Rahul Sharma', desig: 'Practioner', mob: 9471231656,
-        email: 'rahul9@gmail.com', loc: 'Sattarkataia', mgrname: 'Pallwi Singh', mid: 5
-      },
-      {
-        eid: 1, ename: 'Muneswar Sharma', desig: 'programmer', mob: 9571931656,
-        email: 'shrma_i9@gmail.com', loc: 'Channai', mgrname: 'S.RadhaKrishna', mid: 2
-      },
-      {
-        eid: 1, ename: 'Madhkar Nath', desig: 'Practioner', mob: 9471331656,
-        email: 'madhukar39@gmail.com', loc: 'Mugalsarai', mgrname: 'K Kiran', mid: 6
-      },
-      {
-        eid: 1, ename: 'Anita Goswami', desig: 'programmer', mob: 9123931656,
-        email: 'goswami876@gmail.com', loc: 'Pondicherry', mgrname: 'Pallwi Singh', mid: 6
-      },
-      {
-        eid: 1, ename: 'Shalwi Mishra', desig: 'Practioner', mob: 9471654656,
-        email: 'shanwi9@gmail.com', loc: 'Chennai', mgrname: 'S.RadhaKrishna', mid: 5
-      },
-      {
-        eid: 1, ename: 'Muskan', desig: 'Singer', mob: 9471000656,
-        email: 'musjan@gmail.com', loc: 'Chennai', mgrname: 'K.Shiwani', mid: 1
-      },
-      {
-        eid: 1, ename: 'Anika Jha', desig: 'Practioner', mob: 8101931656,
-        email: 'jhaAnika56@gmail.com', loc: 'Gurgoan', mgrname: 'Prakash Khan', mid: 2
-      },
-      {
-        eid: 1, ename: 'Anand Kishore', desig: 'Designer', mob: 9472221656,
-        email: 'kishore_anand@gmail.com', loc: 'Darbhanga', mgrname: 'Pallwi Singh', mid: 3
-      },
-      {
-        eid: 1, ename: 'Tarikh Anwar', desig: 'Developer', mob: 9471131656,
-        email: 'anwar_tarikh@gmail.com', loc: 'Jehanabad', mgrname: 'Pallwi Singh', mid: 6
-      },
-      {
-        eid: 1, ename: 'Manish Keshri', desig: 'HR', mob: 9471900656,
-        email: 'manish1991@gmail.com', loc: 'Jehanabad', mgrname: 'K.Shiwani', mid: 5
-      },
-      {
-        eid: 1, ename: 'Md. Ishwar', desig: 'HR', mob: 9471931336,
-        email: 'eeshwar76@gmail.com', loc: 'Begusaray', mgrname: 'Prakash Khan', mid: 4
-      },
-      {
-        eid: 1, ename: 'Md Ahmad', desig: 'Research & Dev', mob: 9471935556,
-        email: 'ahmad49@gmail.com', loc: 'Darbhanga', mgrname: 'K Kiran', mid: 3
-      },
-      {
-        eid: 1, ename: 'Pushkar Keshri', desig: 'Research & Dev', mob: 2371931656,
-        email: 'keshripushkar9@gmail.com', loc: 'Begusaray', mgrname: 'K.Shiwani', mid: 1
-      },
-      {
-        eid: 1, ename: 'Uttpal Kant', desig: 'Writer', mob: 9331931656,
-        email: 'uttpal75@gmail.com', loc: 'Begusaray', mgrname: 'BalaThirumurgan', mid: 2
-      },
-      {
-        eid: 1, ename: 'Ramtilak Thakur', desig: 'Singer', mob: 8101931656,
-        email: 'ramtilak79@gmail.com', loc: 'Munbai', mgrname: 'Pallwi Singh', mid: 5
-      },
-      {
-        eid: 1, ename: 'Sukhdev Tilak', desig: 'Writer', mob: 8971931656,
-        email: 'sukhdev89@gmail.com', loc: 'Jehanabad', mgrname: 'BalaThirumurgan', mid: 6
-      },
-      {
-        eid: 1, ename: 'Mithi Singh', desig: 'Research & Dev', mob: 8471931656,
-        email: 'singhmithi9@gmail.com', loc: 'Gurgoan', mgrname: 'K.Shiwani', mid: 4
-      },
-      {
-        eid: 1, ename: 'Chanda Kashyap', desig: 'Writer', mob: 9871931656,
-        email: 'kashyap9@gmail.com', loc: 'Munbai', mgrname: 'K.Shiwani', mid: 2
-      },
-      {
-        eid: 1, ename: 'Sindhu Thakur', desig: 'programmer', mob: 9071931656,
-        email: 'sindhu29@gmail.com', loc: 'Gurgoan', mgrname: 'BalaThirumurgan', mid: 3
-      },
-      {
-        eid: 1, ename: 'Ankita', desig: 'Research & Dev', mob: 9471931444,
-        email: 'ankita39@gmail.com', loc: 'Munbai', mgrname: 'Pallwi Singh', mid: 1
-      }
+  {
+    eid: 1, ename: 'Tom Gorge', desig: 'Developer', mob: 9471931608,
+    email: 'gorge@gmail.com', loc: 'Keral', mgrname: 'L. Lathia', mid: 1
+},
+{
+    eid: 2, ename: 'Nihar Ranjan', desig: 'Designer', mob: 9371932347,
+    email: 'nihar342@gmail.com', loc: 'Darbhanga', mgrname: 'Shasikant Sinde', mid: 2
+},
+{
+    eid: 3, ename: 'Anand Kumar', desig: 'Programmer', mob: 8871931623,
+    email: 'anandee3@gmail.com', loc: 'Madhipura', mgrname: 'L. Lathia', mid: 4
+},
+{
+    eid: 4, ename: 'K Kasturba', desig: 'Developer', mob: 9477771608,
+    email: 'kasturba4@gmail.com', loc: 'Supaul', mgrname: 'Purushottam Singh', mid: 4
+},
+{
+    eid: 5, ename: 'Khushboo Mishra', desig: 'Seveloper', mob: 9471881611, email:
+        'khushboo45@gmail.com', loc: 'Varanasi', mgrname: 'L. Lathia', mid: 3
+},
+{
+    eid: 6, ename: 'Shilpa Jha', desig: 'Team Leader', mob: 9071931608,
+    email: 'employee6@gmail.com', loc: 'Karaikal', mgrname: 'Shasikant Sinde', mid: 3
+},
+{
+    eid: 7, ename: 'Mayanka Singh', desig: 'ProjManager', mob: 7771931608,
+    email: 'mayanka@gmail.com', loc: 'U.P', mgrname: 'Shasikant Sinde', mid: 3
+},
+{
+    eid: 8, ename: 'Prashant', desig: 'Designer', mob: 9471931123,
+    email: 'Prashant8@gmail.com', loc: 'Chattisgadh', mgrname: 'Mahanad Sada', mid: 1
+},
+{
+    eid: 1, ename: 'Kaushal Kumar', desig: 'UI developer', mob: 9471931656,
+    email: 'kaushal12@gmail.com', loc: 'Kolkata', mgrname: 'Shasikant Sinde', mid: 2
+},
+{
+    eid: 9, ename: 'Arvind S', desig: 'Designer', mob: 9471931543,
+    email: 'arvind10@gmail.com', loc: 'Panjab', mgrname: 'Mahanad Sada', mid: 1
+}
+   
 ];
--- src/app/constants/manager-info.ts
+++ src/app/constants/manager-info.ts
@@ -1,11 +1,9 @@
 import { Managerdetail } from '../modals/manager-modal';
 
 export const managers: Managerdetail[] = [
-  { mid: 1, mgname: 'S.RadhaKrishna', mob: '9471931157', email: 'radha12@gmail.com', loc: 'Hyderabad' },
-  { mid: 2, mgname: 'BalaThirumurgan', mob: '9371931333', email: 'thirumurgan91@gmail.com', loc: 'Pune' },
-  { mid: 3, mgname: 'Prakash Khan', mob: '8171931123', email: 'prakash87@gmail.com', loc: 'Benglore' },
-  { mid: 4, mgname: 'K Kiran', mob: '9671931321', email: 'kiran42@gmail.com', loc: 'Delhi' },
-  { mid: 5, mgname: 'Pallwi Singh', mob: '9471900021', email: 'pallwi42@gmail.com', loc: 'Panjab' },
-  { mid: 6, mgname: 'K.Shiwani', mob: '8671342321', email: 'shiwani42@gmail.com', loc: 'Gurgoan' }
- 
+{ mid: 1, mgname: 'Shasikant Sinde', mob: '9471931157', email: 'manager1@gmail.com', loc: 'Hyd_1' },
+{ mid: 2, mgname: 'Mahanad Sada', mob: '9371931333', email: 'manager2@gmail.com', loc: 'Pune_2' },
+{ mid: 3, mgname: 'Purushottam Singh', mob: '9171931123', email: 'manager3@gmail.com', loc: 'Patna_3' },
+{ mid: 4, mgname: 'L. Lathia', mob: '9671931321', email: 'manager4@gmail.com', loc: 'Delhi_4' }
+
   ];
